6da7d0ab77f042e55ba49f6a627f828b5a7e627c
melaibanez <melu.poker@gmail.com>
2020-04-25 11:15:20 -0300

    Muevo variables al .h

--- Broker/src/Broker.h
+++ Broker/src/Broker.h
@@ -10,6 +10,22 @@
 
 #include "utils.h"
 
+
+//agrego las variables que se agregan en el archivo de conf ademas del ip y puerto
+int* TAMANO_MEMORIA;
+int* TAMANO_MINIMO_PARTICION;
+char* ALGORITMO_MEMORIA;
+char* ALGORITMO_REEMPLAZO;
+char* ALGORITMO_PARTICION_LIBRE;
+char* IP_BROKER;
+int* PUERTO_BROKER;
+int* FRECUENCIA_COMPACTACION;
+char* LOG_FILE;
+
+t_log* logger;
+t_config* config;
+
+
 t_log* iniciar_logger(void);
 t_config* leer_config(void);
 void terminar_programa(int, t_log*, t_config*);
