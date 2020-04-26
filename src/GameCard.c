f90489b99871ae72cf65a43a2e7d8c182f1d8c6d
delficostanzo <delfi.costanzo@hotmail.com>
2020-04-26 16:27:13 -0300

    Modifico un par de cosas de los modulos GameBoy, GameCard y Team y agrego los archivos .config

--- src/GameCard.c
+++ src/GameCard.c
@@ -16,82 +16,64 @@
 int main(void) {
 	int conexion;
 
-	//agrego las variables que se agregan en el archivo de conf ademas del ip y puerto
-		int* TIEMPO_DE_REINTENTO_CONEXION;
-		int* TIEMPO_DE_REINTENTO_OPERACION;
-		char* PUNTO_MONTAJE_TALLGRASS;
-		char* IP_BROKER;
-		int* PUERTO_BROKER;
+	// inicializo el log del Broker
+	logger = iniciar_logger();
 
-		t_log* logger;
-		t_config* config;
+	// creo y devuelvo un puntero a la estructura t_config
+	config = leer_config();
 
-		// inicializo el log del Broker
-		logger = iniciar_logger();
+	TIEMPO_DE_REINTENTO_CONEXION = config_get_int_value(config, "TIEMPO_DE_REINTENTO_CONEXION");
+	TIEMPO_DE_REINTENTO_OPERACION = config_get_int_value(config, "TIEMPO_DE_REINTENTO_OPERACION");
+	PUNTO_MONTAJE_TALLGRASS = config_get_string_value(config, "PUNTO_MONTAJE_TALLGRASS");
+	IP_BROKER = config_get_string_value(config, "IP_BROKER");
+	PUERTO_BROKER = config_get_int_value(config, "PUERTO_BROKER");
 
-		// creo y devuelvo un puntero a la estructura t_config
-		config = leer_config();
+	// faltaria loggear la info de todo el archivo de configuracion, ademas de ip y puerto
+	log_info(logger, "Lei la IP %s y PUERTO %s\n", IP_BROKER, PUERTO_BROKER);
 
-		TIEMPO_DE_REINTENTO_CONEXION = config_get_int_value(config, "TIEMPO_DE_REINTENTO_CONEXION");
-		TIEMPO_DE_REINTENTO_OPERACION = config_get_int_value(config, "TIEMPO_DE_REINTENTO_OPERACION");
-		PUNTO_MONTAJE_TALLGRASS = config_get_string_value(config, "PUNTO_MONTAJE_TALLGRASS");
-		IP_BROKER = config_get_string_value(config, "IP_BROKER");
-		PUERTO_BROKER = config_get_int_value(config, "PUERTO_BROKER");
-
-		// faltaria loggear la info de todo el archivo de configuracion, ademas de ip y puerto
-		log_info(logger, "Lei la IP %s y PUERTO %s\n", IP_BROKER, PUERTO_BROKER);
-
-		//crear conexion, un socket conectado
-
-		conexion = crear_conexion(IP_BROKER, PUERTO_BROKER);
-
-		// envio mensaje (el primer parametro esta vacio porque depende de cada estructura de mensaje, va un valor distinto). No lo completo porque vicky no commiteo al master
-		enviar_mensaje(, conexion);
-
-		// recibir mensaje
-		char* mensaje = recibir_mensaje(conexion);
-
-		//loguear mensaje recibido
-
-		log_info(logger, "El mensaje recibido es: %s\n", mensaje);
-
-		terminar_programa(conexion, logger, config);
+	// envio mensaje (el primer parametro esta vacio porque depende de cada estructura de mensaje, va un valor distinto). No lo completo porque vicky no commiteo al master
+	//enviar(pack,socket_cliente);
 
 
-	}
+	// recibir mensaje
+	//t_paquete* mensaje = recibir_mensaje(conexion); //lo recibimos y la funcion recibir mensaje lo mete en un paquete
 
-	t_log* iniciar_logger(void){
-		t_log* logger = log_create("gamecard.log", "GAMECARD", 1,LOG_LEVEL_INFO);
+	//loguear mensaje recibido
+
+	//log_info(logger, "El mensaje recibido es: %s\n", mensaje);
+
+	//terminar_programa(conexion, logger, config);
+}
+
+t_log* iniciar_logger(void){
+	t_log* logger = log_create("gamecard.log", "GAMECARD", 1,LOG_LEVEL_INFO);
 		if(logger == NULL){
 				printf("No pude crear el logger \n");
 				exit(1);
 		}
 		return logger;
-	}
+}
 
-	t_config* leer_config(void)
-	{
-		t_config* config = config_create("./gamecard.config");
-
+t_config* leer_config(void)
+{
+	t_config* config = config_create("src/gamecard.config");
 		if(config == NULL){
-			printf("No pude leer la config \n");
-			exit(2);
-		}
-		return config;
+		printf("No pude leer la config \n");
+		exit(2);
+	}
+	return config;
+}
+
+void terminar_programa(int conexion, t_log* logger, t_config* config)
+{
+	if(logger != NULL){
+		log_destroy(logger);
+	}
+	if(config != NULL){
+		config_destroy(config); //destruye la esctructura de config en memoria, no lo esta eliminando el archivo de config
 	}
 
+	//liberar_conexion(conexion); // esta funcion esta en utils.c
 
-	void terminar_programa(int conexion, t_log* logger, t_config* config)
-	{
-		if(logger != NULL){
-			log_destroy(logger);
-		}
-
-		if(config != NULL){
-			config_destroy(config); //destruye la esctructura de config en memoria, no lo esta eliminando el archivo de config
-		}
-
-		liberar_conexion(conexion); // esta funcion esta en utils.c
-
-	}
+}
 
