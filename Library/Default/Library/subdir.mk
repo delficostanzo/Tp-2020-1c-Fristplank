b7729e9b552cba12f8df1220431aa9eda39292a2
delficostanzo <delfi.costanzo@hotmail.com>
2020-04-29 19:26:38 -0300

    Agrego la carpeta Debug al .gitignore

--- Library/Default/Library/subdir.mk
+++ Library/Default/Library/subdir.mk
@@ -4,20 +4,20 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
-../src/Team.c 
+../Library/utils.c 
 
 OBJS += \
-./src/Team.o 
+./Library/utils.o 
 
 C_DEPS += \
-./src/Team.d 
+./Library/utils.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
-src/%.o: ../src/%.c
+Library/%.o: ../Library/%.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C Compiler'
-	gcc -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	gcc -O2 -g -Wall -c -fmessage-length=0 -fPIC -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
