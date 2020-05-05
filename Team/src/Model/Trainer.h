82fde8b4fb45882de71bea7ee85661005889eee5
victoriagarciaa <vicky.g@live.com.ar>
2020-05-04 22:30:57 -0300

    Creacion del entrenador

---
+++ Team/src/Model/Trainer.h
@@ -0,0 +1,17 @@
+/*
+ * Trainer.h
+ *
+ *  Created on: 4 may. 2020
+ *      Author: utnso
+ */
+
+#ifndef SRC_MODEL_TRAINER_H_
+#define SRC_MODEL_TRAINER_H_
+
+#include "utils.h"
+
+typedef struct {
+	t_posicion* posicion;
+}t_trainer;
+
+#endif /* SRC_MODEL_TRAINER_H_ */
