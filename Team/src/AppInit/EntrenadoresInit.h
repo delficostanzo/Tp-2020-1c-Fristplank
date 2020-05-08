bf3c909fbaf3b5b286100d4db561d92d58bb8115
victoriagarciaa <vicky.g@live.com.ar>
2020-05-08 02:26:12 -0300

    Agregamos cambios y se obtienen los objetivos globales

--- Team/src/AppInit/EntrenadoresInit.h
+++ Team/src/AppInit/EntrenadoresInit.h
@@ -11,5 +11,6 @@
 #include "utils.h"
 
 t_list* getEntrenadoresDesde(String nombreDeArchivo);
+t_list* getObjetivosGlobalesDesde(t_list* entrenadores);
 
 #endif /* SRC_APPINIT_ENTRENADORESINIT_H_ */
