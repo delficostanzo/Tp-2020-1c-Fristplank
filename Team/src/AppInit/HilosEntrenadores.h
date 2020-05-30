a30537fc791f6322d85ffb46eb0abaf752b16b73
delficostanzo <delfi.costanzo@hotmail.com>
2020-05-30 20:28:49 -0300

    Estado NEW terminado y reformas en los hilos

--- Team/src/AppInit/HilosEntrenadores.h
+++ Team/src/AppInit/HilosEntrenadores.h
@@ -4,9 +4,13 @@
 #define SRC_APPINIT_HILOSENTRENADORES_H_
 
 #include "utils.h"
+#include "../Model/Trainer.h"
+#include "../TeamUtils/LogManager.h"
+#include "ConfigFunctions.h"
+#include "Planificador.h"
 
 typedef void* Data;
 
-void crearHilosDeEntrenadores(t_list* entrenadores, pthread_t* hilos[]);
+void crearHilosDeEntrenadores(t_list* entrenadores);
 
 #endif /* SRC_APPINIT_HILOSENTRENADORES_H_ */
