2a1453d3ce703d90abf7542109315c898ac39377
victoriagarciaa <vicky.g@live.com.ar>
2020-06-03 19:04:14 -0300

    Se corrigen detalles en la conexion y cambiamos los int por uint32

--- utils.h
+++ utils.h
@@ -37,53 +37,54 @@ typedef enum
 
 typedef struct
 {
-	int posicionX;
-	int posicionY;
+	uint32_t posicionX;
+	uint32_t posicionY;
 } t_posicion;
 
 typedef struct
 {
-	int lengthOfPokemon;
+	uint32_t lengthOfPokemon;
 	char* pokemon;
 	t_posicion* posicion;
-	int cantidad;
+	uint32_t cantidad;
 } t_new_pokemon;
 
 typedef struct
 {
-	int lengthOfPokemon;
+	uint32_t lengthOfPokemon;
 	char* pokemon;
 	t_posicion* posicion;
 } t_appeared_pokemon;
 
 typedef struct
 {
-	int lengthOfPokemon;
+	uint32_t lengthOfPokemon;
 	char* pokemon;
 	t_posicion* posicion;
 } t_catch_pokemon;
 
 typedef struct
 {
-	bool ok;
+	uint32_t ok;
 } t_caught_pokemon;
 
 typedef struct
 {
-	int lengthOfPokemon;
+	uint32_t lengthOfPokemon;
 	char* pokemon;
 } t_get_pokemon;
 
 typedef struct
 {
-	int lengthOfPokemon;
+	uint32_t lengthOfPokemon;
 	char* pokemon;
+	uint32_t cantidadPosiciones;
 	t_list* listaPosiciones;
 } t_localized_pokemon;
 
 typedef struct
 {
-	int idCorrelativo;
+	uint32_t idCorrelativo;
 } t_respuesta_id;
 
 typedef struct
@@ -95,20 +96,20 @@ typedef struct
 typedef struct
 {
 	op_code codigo_operacion;
-	int ID;
-	int ID_CORRELATIVO;
+	uint32_t ID;
+	uint32_t ID_CORRELATIVO;
 	t_buffer* buffer;
 } t_paquete;
 
-int crear_conexion(char* ip, char* puerto);
+uint32_t crear_conexion(char* ip, char* puerto);
 
-void enviar_new_pokemon(t_new_pokemon* new_pokemon, int socket_cliente, int ID, int IDCORRELATIVO);
-void enviar_appeared_pokemon(t_appeared_pokemon* appeared_pokemon, int socket_cliente, int ID, int IDCORRELATIVO);
-void enviar_catch_pokemon(t_catch_pokemon* catch_pokemon, int socket_cliente, int ID, int IDCORRELATIVO);
-void enviar_caught_pokemon(t_caught_pokemon* caught_pokemon, int socket_cliente, int ID, int IDCORRELATIVO);
-void enviar_get_pokemon(t_get_pokemon* get_pokemon, int socket_cliente, int ID, int IDCORRELATIVO);
-void enviar_localized_pokemon(t_localized_pokemon* localized_pokemon, int socket_cliente, int ID, int IDCORRELATIVO);
-void enviar_ACK(int socket_cliente, int ID, int IDCORRELATIVO);
+void enviar_new_pokemon(t_new_pokemon* new_pokemon, int socket_cliente, uint32_t ID, uint32_t IDCORRELATIVO);
+void enviar_appeared_pokemon(t_appeared_pokemon* appeared_pokemon, int socket_cliente, uint32_t ID, uint32_t IDCORRELATIVO);
+void enviar_catch_pokemon(t_catch_pokemon* catch_pokemon, int socket_cliente, uint32_t ID, uint32_t IDCORRELATIVO);
+void enviar_caught_pokemon(t_caught_pokemon* caught_pokemon, int socket_cliente, uint32_t ID, uint32_t IDCORRELATIVO);
+void enviar_get_pokemon(t_get_pokemon* get_pokemon, int socket_cliente, uint32_t ID, uint32_t IDCORRELATIVO);
+void enviar_localized_pokemon(t_localized_pokemon* localized_pokemon, int socket_cliente, uint32_t ID, uint32_t IDCORRELATIVO);
+void enviar_ACK(int socket_cliente, uint32_t ID, uint32_t IDCORRELATIVO);
 
 t_paquete* recibir_mensaje(int socket_cliente);
 
