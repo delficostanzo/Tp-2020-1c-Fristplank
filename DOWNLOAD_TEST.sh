30041aaa5c3f18935e81ab3e63497da18fed811f
Guido Ferrari <guido_ferrari@live.com>
2020-07-18 08:27:25 -0300

    Download test

--- DOWNLOAD_TEST.sh
+++ DOWNLOAD_TEST.sh
@@ -1,12 +1,11 @@
-	if test -d /home/utnso/delibird-pruebas; then
-		cd /home/utnso/
-		sudo rm -r /delibird-pruebas
+	if test -d /home/utnso/delibird-pruebas/; then
+		sudo rm -r /home/utnso/delibird-pruebas/
 	fi
-cd
 
-	cd home/utnso
+cd
+	cd /home/utnso/
 	git clone https://github.com/sisoputnfrba/delibird-pruebas.git
-	cd home/utnso/delibird-pruebas
-	\cp * /home/utnso/tp-2020-1c-Fritsplank/GameBoy/Debug
+	cd /home/utnso/delibird-pruebas/
+	\cp *.sh /home/utnso/tp-2020-1c-Fritsplank/GameBoy/Debug
 
 cd
