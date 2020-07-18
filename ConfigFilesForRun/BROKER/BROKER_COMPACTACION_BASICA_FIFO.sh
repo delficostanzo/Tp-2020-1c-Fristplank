6967f039e00b7a60efb4549c9bc07b7234ebb058
Guido Ferrari <guido_ferrari@live.com>
2020-07-18 08:49:16 -0300

    Add sh

--- ConfigFilesForRun/BROKER/BROKER_COMPACTACION_BASICA_FIFO.sh
+++ ConfigFilesForRun/BROKER/BROKER_COMPACTACION_BASICA_FIFO.sh
@@ -1,6 +1,6 @@
 cd
 
-	cd /home/utnso/tp-2020-1c-Fritsplank/ConfigFilesForRun/BROKER/BROKER_COMPACTACION_BASICA_FIFO
+	cd /home/utnso/tp-2020-1c-Fritsplank/ConfigFilesForRun/BROKER/BROKER_COMPACTACION_BASICA_FIFO/
 	\cp broker.config /home/utnso/tp-2020-1c-Fritsplank/Broker/src/
 
 cd
